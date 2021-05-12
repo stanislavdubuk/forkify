5c1ff2bd02467659ca5b5fd16ef0dd711482c9d9
stanislavdubuk <stanislavdubuk@gmail.com>
2021-05-12 13:33:54 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e23c04d6-4fb6-42c6-a897-b33a51517504';
+export const MODAL_CLOSE_SEC = 2.5;
