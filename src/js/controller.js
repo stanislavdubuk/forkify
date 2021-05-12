e557b881de0b0f62fe9e0735c0d99b6f1a9d4775
stanislavdubuk <stanislavdubuk@gmail.com>
2021-05-12 13:59:38 +0300

    Added  new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,6 +126,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome!');
+};
+
 // publisher subscriber pattern
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -135,5 +139,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
